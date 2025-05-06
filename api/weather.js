5139f8c226bd957866e114ef6b17138d69992df7
AranthaG <aranthashreyagogoi@gmail.com>
2025-05-07 01:21:10 +0530

    git commit -m "feat: add weather API integration"

---
+++ api/weather.js
@@ -0,0 +1,15 @@
+export default async function handler(req, res) {
+    const city = req.query.city || 'Lucknow';
+    const apiKey = process.env.WEATHER_API_KEY;
+  
+    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+  
+    try {
+      const response = await fetch(apiUrl);
+      const data = await response.json();
+      res.status(200).json(data);
+    } catch (error) {
+      res.status(500).json({ error: "Unable to fetch weather data" });
+    }
+  }
+  
