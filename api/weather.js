7207209b64d2437b11996235aeb233b4ff180ac1
Arantha Shreya <aranthashreyagogoi@gmail.com>
2025-05-07 01:21:21 +0530

    Update weather.js

--- api/weather.js
+++ api/weather.js
@@ -1,8 +1,9 @@
 export default async function handler(req, res) {
     const city = req.query.city || 'Lucknow';
     const apiKey = process.env.WEATHER_API_KEY;
-  
-    const apiUrl = `http://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${city}&aqi=no`;
+
+    const apiKey = process.env.WEATHER_API_KEY;
+
   
     try {
       const response = await fetch(apiUrl);
